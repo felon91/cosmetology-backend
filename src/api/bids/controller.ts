d421ebbeadb01d617cb18614e36bdad0626a33e3
Denis Bulyha <felon_91@mail.ru>
2022-09-24 17:50:57 +0300

    fix(core): 🔨 example

--- src/api/bids/controller.ts
+++ src/api/bids/controller.ts
@@ -1,10 +1,27 @@
+import { createTransport } from 'nodemailer';
+
 import { fail } from 'lib/core';
-import { sendMail } from 'lib/mailer';
 
 import { Bid } from './model';
 
 import type { BidProps } from 'api/bids/core';
 import type { Response } from 'express';
+
+const transporter = createTransport({
+  host: '"smtp.yandex.ru',
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const getMailOptions = () => ({
+  from: 'firebooom@yandex.by', // Sender address
+  to: 'felon1991@yandex.ru', // List of recipients
+  subject: 'Заявка с сайта', // Subject line
+  html: 'This <i>message</i> with <strong>attachments</strong>.',
+});
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const saveBid = (res: Response, data: BidProps) => {
@@ -16,7 +33,7 @@ export const saveBid = (res: Response, data: BidProps) => {
         .status(200)
         .json({ ...fail, errors: [{ field: 'phone', msg: 'Некорректный телефон' }] });
     }
-    void sendMail();
+    void transporter.sendMail(getMailOptions());
     return res.status(200).json({ success: true, body: null, errors: null });
   });
 };
